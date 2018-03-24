b9114f4ff0c63118589e852bf04e07106583a014
IntSPstudio <official.intsp@gmail.com>
2018-03-24 20:34:47 +0200

    0.0.3.20172403 - 67.7200.318-2403.2.1
    More fixing

--- timeleap.bat
+++ timeleap.bat
@@ -2,32 +2,49 @@
 REM Made by IntSPstudio
 REM Time Leap
 REM Thank you for using this software!
-REM Version: 0.0.1.20172403
+REM Version: 0.0.3.20172403
 REM ID: 720002001
-REM
 REM Twitter: @IntSPstudio
 
 REM SETTINGS
+echo Settings
 setlocal enabledelayedexpansion
-set /p timeinterval="Time intervall: "
-set /p loopcounter="Loop counter: "
+set /p timeinterval="-Time interval: "
+set /p loopcounter="-Loop counter: "
+set /p counterleap=" -Leap length: "
 set currenthour=%time: =0%
 set /a currenthour=%currenthour:~0,2%
 set /a debugger=0
 REM MAIN LOOP
+echo.
+set /a fhh=%currenthour%
 for /l %%i in (1,1,%loopcounter%) do (
 	REM 'FAKE' HOUR VALUE
-	set /a fhh=%currenthour%+%%~ni
+	set /a check=!fhh!+%counterleap%
+	set /a fhh+=%counterleap%
+	if !check! geq 24 (
+		set /a fhh-=24
+	)
 	REM GET TIME
 	set ctime=!fhh!.!time:~3,2!.!time:~6,2!,!time:~9,2!
 	REM CHANGE TIME
-	if %debugger%==0 (time !ctime!)
+	if %debugger%==0 (
+		time !ctime!
+	) else (
+		echo !ctime!
+	)
 	REM PAUSE
 	timeout /t %timeinterval%
 )
 REM BACK TO NORMAL
 echo Changing time to normal
+if %debugger%==0 (
 pause
+)
 set /a fhh=%currenthour%
 set ltime=!fhh!.!time:~3,2!.!time:~6,2!,!time:~9,2!
-if %debugger%==0 (time !ltime!)
+if %debugger%==0 (
+	time !ltime!
+) else (
+	echo !ltime!
+)
